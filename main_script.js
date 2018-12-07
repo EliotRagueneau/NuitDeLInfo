65670af54ffdb8c9c0213f93380bb0cc4919fb1b
Orkeliot <eliot.ragueneau@etu.u-bordeaux.fr>
2018-12-07 03:52:02 +0100

    police

--- main_script.js
+++ main_script.js
@@ -1,8 +1,3 @@
-$("#play").click(function(){
-  var x = document.getElementById("audio");
-  x.play();
-  // $("#audio").play();
-  // $("#audio").play();
-  // $("#audio")[id-1].pause();
-  // console.log("coucou");
+$("#life").click(function () {
+    $("main").html("<h2>Life</h2>");
 });
