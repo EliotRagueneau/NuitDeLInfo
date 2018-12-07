f301d686628e02ed5bc6a3e180055f6588c22407
Charlotte André <charlotte-andre@hotmail.fr>
2018-12-07 05:45:13 +0100

    maj id

--- main_script.js
+++ main_script.js
@@ -17,6 +17,37 @@ $("#weather").click(function () {
     goIn();
 });
 
+$("#battery").click(function () {
+    goOut();
+
+    $(".main").html("<h2>Warning low battery</h2>");
+
+    goIn();
+});
+
+$("#low battery").click(function () {
+    goOut();
+
+    $(".main").html("<h2>No more battery</h2>");
+
+    goIn();
+});
+
+$("#whathappend").click(function () {
+    goOut();
+
+    $(".main").html("<h2>How is the world today</h2>");
+
+    goIn();
+});
+
+$("#bodyguard").click(function () {
+    goOut();
+
+    $(".main").html("<h2>withney houston</h2>");
+
+    goIn();
+});
 function goOut() {
     if (first_click) {
         first_click = false
